22caef38e62ed52212114974b69e4aa7c2a1d242
yeison hernandez <yeison.hernandez@grupodigital.co>
2021-02-16 14:59:21 -0500

    generrar secureRandom6

--- lib/src/crypto.dart
+++ lib/src/crypto.dart
@@ -51,4 +51,20 @@ class CryptoApi {
 
     return sig.bytes;
   }
+
+  bool rsaVerify(
+      RSAPublicKey publicKey, Uint8List signedData, Uint8List signature) {
+    final sig = RSASignature(signature);
+
+    final verifier = RSASigner(SHA256Digest(), '0609608648016503040201');
+
+    verifier.init(
+        false, PublicKeyParameter<RSAPublicKey>(publicKey)); // false=verify
+
+    try {
+      return verifier.verifySignature(signedData, sig);
+    } on ArgumentError {
+      return false; // for Pointy Castle 1.0.2 when signature has been modified
+    }
+  }
 }
