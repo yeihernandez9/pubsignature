14d670f23a284ce5b1babd183b33d360d195398b
yeison hernandez <yeison.hernandez@grupodigital.co>
2021-02-16 13:09:51 -0500

    generrar secureRandom

--- lib/src/crypto.dart
+++ lib/src/crypto.dart
@@ -25,4 +25,17 @@ class Crypto {
 
     return AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>(myPublic, myPrivate);
   }
+
+  SecureRandom exampleSecureRandom() {
+    final secureRandom = FortunaRandom();
+
+    final seedSource = Random.secure();
+    final seeds = <int>[];
+    for (int i = 0; i < 32; i++) {
+      seeds.add(seedSource.nextInt(255));
+    }
+    secureRandom.seed(KeyParameter(Uint8List.fromList(seeds)));
+
+    return secureRandom;
+  }
 }
